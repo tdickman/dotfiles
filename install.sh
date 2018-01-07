14d535c29ba0f56ccc65767d8b47a5cf3570518e
Tom Dickman <tdickman@gmail.com>
2018-01-07 17:50:36 -0600

    Added jshint

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -s $SCRIPTPATH/vim ~/.vim
 ln -s $SCRIPTPATH/tmux.conf ~/.tmux.conf
 ln -s $SCRIPTPATH/bash_aliases.sh ~/.bash_aliases.sh
 ln -s $SCRIPTPATH/bash_shared.sh ~/.bash_shared.sh
+ln -s $SCRIPTPATH/.jshintrc ~/.jshintrc
 sudo ln -s $SCRIPTPATH/pykub.py /usr/local/bin/pykub
 sudo chmod +x /usr/local/bin/pykub
 ln -s ~/.vim/vimrc ~/.vimrc
