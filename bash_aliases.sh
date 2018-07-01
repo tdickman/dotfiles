fa4ecace34966db10affb844560da3c9988e6b86
Tom Dickman <tdickman@gmail.com>
2018-07-01 13:51:21 -0500

    Added scripts for vpn connections

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -177,3 +177,17 @@ function ssh-yubi {
     ssh -R /run/user/1000/gnupg/S.gpg-agent.ssh:/run/user/1000/gnupg/S.gpg-agent.ssh $1
 }
 export -f ssh-yubi
+function vpnlocal {
+    echo $'vpn\n'$(pass 192.168.1.1/vpn | head -n 1) > /tmp/vpn
+    chmod 600 /tmp/vpn
+    sudo openvpn --config Dropbox/VPN/home.epicconstructions.com.local.ovpn --auth-user-pass /tmp/vpn
+    rm /tmp/vpn
+}
+export -f vpnlocal
+function vpnall {
+    echo $'vpn\n'$(pass 192.168.1.1/vpn | head -n 1) > /tmp/vpn
+    chmod 600 /tmp/vpn
+    sudo openvpn --config Dropbox/VPN/home.epicconstructions.com.all.ovpn --auth-user-pass /tmp/vpn
+    rm /tmp/vpn
+}
+export -f vpnall
