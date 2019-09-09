9dd51b6b45bb31844eb2aaa4ece60d5bcabc443b
Tom Dickman <tom.dickman@foldapp.com>
2019-09-09 16:45:44 -0400

    Make uencrypted work from user account

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -168,7 +168,7 @@ export -f yubi-remote-clean
 
 # Mount encrypted dir
 alias mencrypted='pass show encrypted-dir | head -n 1 | encfs -S ~/Drive/Encrypted ~/Encrypted'
-alias uencrypted='sudo umount ~/Encrypted'
+alias uencrypted='fusermount -u ~/Encrypted'
 alias gc='pass show encrypted-dir | head -n 1 | encfs -S ~/Drive/Encrypted ~/Encrypted && xdg-open Encrypted/giftcards/reselling.ods && echo "Press enter to unmount volume" && read _ && sudo umount ~/Encrypted'
 alias start_torrent="~/dotfiles/start_torrent.sh"
 alias dc="docker-compose"
