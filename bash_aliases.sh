ea2f77837558f1ef8fbfa4b8d6313542f1ed9dfa
Tom Dickman <tdickman@gmail.com>
2020-02-17 12:12:08 -0500

    Add bash aliases for starting local cluster

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -271,3 +271,43 @@ function poetryenv {
     ln -s $(poetry env list --full-path | head -n 1 | awk '{print $1;}') $(pwd)/.venv
 }
 export -f poetryenv
+
+function kindup {
+    # desired cluster name; default is "kind"
+    KIND_CLUSTER_NAME="${KIND_CLUSTER_NAME:-kind}"
+
+    # create registry container unless it already exists
+    reg_name='kind-registry'
+    reg_port='5000'
+    running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
+    if [ "${running}" != 'true' ]; then
+      docker run \
+        -d --restart=always -p "${reg_port}:5000" --name "${reg_name}" \
+        registry:2
+    fi
+    reg_ip="$(docker inspect -f '{{.NetworkSettings.IPAddress}}' "${reg_name}")"
+
+    # create a cluster with the local registry enabled in containerd
+cat <<EOF | kind create cluster --name "${KIND_CLUSTER_NAME}" --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+containerdConfigPatches: 
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
+    endpoint = ["http://${reg_ip}:${reg_port}"]
+EOF
+
+    krc kind-kind a-kind
+    kubectl config set default-report localhost:5000
+    kubectl config set local-cluster false
+}
+export -f kindup
+
+function kinddown {
+    kind delete cluster
+    reg_name='kind-registry'
+    docker stop $reg_name
+    docker rm $reg_name
+    kdc a-kind || true
+}
+export -f kinddown
